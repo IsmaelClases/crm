e029f18aa0faa54187ba2b210fe04f13b45b8dac
IsmaelClases <131903894+IsmaelClases@users.noreply.github.com>
2023-11-28 18:52:36 +0100

    añadiendo enviornment

---
+++ Environment/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true
+};
+export const URL_API = 'http://143.47.38.72/dev/appMontsalvez';
+//export const URL_API = 'http://143.47.38.3/dev/api/private';
+export const URL_BASE = 'http://143.47.38.72';
